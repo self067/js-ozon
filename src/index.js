72bf495af7365cd33f9de32ffd191f7cdabb7dc1
unknown <seltor@mail.ru>
2019-08-14 22:52:25 +0300

    20190814-4

--- src/index.js
+++ src/index.js
@@ -89,11 +89,10 @@ function addCart() {
 
 	function showData() {
 		const cardsCart = cartWrap.querySelectorAll('.card'),
-			cardsPrice = cartWrap.querySelectorAll('.card-price'),
-			cardTotal = document.querySelector('.cart-total span');
+					cardsPrice = cartWrap.querySelectorAll('.card-price'),
+					cardTotal = document.querySelector('.cart-total span');
 
 		countGoods.textContent = cardsCart.length;
-
 		let sum = 0;
 
 		cardsPrice.forEach((cardPrice) => {
@@ -124,60 +123,88 @@ function filterActions() {
 		search = document.querySelector('.search-wrapper_input'),
 		searchBtn = document.querySelector('.search-btn');
 
+	function filterAll (){
 
-	// фильтр по акции
-	discountCheckbox.addEventListener('click', () => {
-		cards.forEach((card) => {
-			if (discountCheckbox.checked) {
-				if (!card.querySelector('.card-sale')) {
-					card.parentNode.style.display = 'none';
-					// card.parentNode.remove();
-				}
-			} else {
-				card.parentNode.style.display = '';
-				// document.querySelector('.goods').appendChild(card.parentNode);
-			}
-		});
-	});
-
-
-	min.addEventListener('change', filterPrice);
-	max.addEventListener('change', filterPrice);
-
-	function filterPrice() {
 		cards.forEach((card) => {
 			const cardPrice = card.querySelector('.card-price');
 			const price = parseFloat(cardPrice.textContent);
-			// console.log(price);
-			// console.log(min.value);
-			// console.log(max.value);
+			const searchText = new RegExp(search.value.trim(), 'i'); // i - case insensitive
+			const title = card.querySelector('.card-title');
 
-			if ((min.value && price < min.value) || (max.value && price > max.value)) {
-				card.parentNode.remove();
-				// card.parentNode.style.display = 'none';				
-			} else {
-				goods.appendChild(card.parentNode);
-				// card.parentNode.style.display = '';
-			}
+			if (  ((min.value && price < min.value) || (max.value && price > max.value)) ||
+			      ( discountCheckbox.checked && !card.querySelector('.card-sale')) ||
+						( search.value.trim() && !searchText.test(title.textContent)) 
+
+			) {
+					card.parentNode.style.display = 'none';
+						// card.parentNode.remove();
+				} else {
+					card.parentNode.style.display = '';
+					// document.querySelector('.goods').appendChild(card.parentNode);
+				}
 		});
 	}
+////////////////////////////////////////////////			
+
+	discountCheckbox.addEventListener('click', filterAll);
+	min.addEventListener('change', filterAll);
+	max.addEventListener('change', filterAll);
+
+	searchBtn.addEventListener('click', filterAll);
 
 
-	// поиск
-	searchBtn.addEventListener('click', () => {
-		const searchText = new RegExp(search.value.trim(), 'i'); // i - case insensitive
-		console.log(searchText);
-		cards.forEach((card) => {
-			const title = card.querySelector('.card-title');
-			if (!searchText.test(title.textContent)) {
-				card.parentNode.style.display = 'none';
-			} else {
-				card.parentNode.style.display = '';
+	// // фильтр по акции
+	// discountCheckbox.addEventListener('click', () => {
+	// 	cards.forEach((card) => {
+	// 		if (discountCheckbox.checked) {
+	// 			if (!card.querySelector('.card-sale')) {
+	// 				card.parentNode.style.display = 'none';
+	// 				// card.parentNode.remove();
+	// 			}
+	// 		} else {
+	// 			card.parentNode.style.display = '';
+	// 			// document.querySelector('.goods').appendChild(card.parentNode);
+	// 		}
+	// 	});
+	// });
 
-			}
-		});
-		search.value = '';
-	});
+	// min.addEventListener('change', filterPrice);
+	// max.addEventListener('change', filterPrice);
+
+	// function filterPrice() {
+	// 	cards.forEach((card) => {
+	// 		const cardPrice = card.querySelector('.card-price');
+	// 		const price = parseFloat(cardPrice.textContent);
+	// 		// console.log(price);
+	// 		// console.log(min.value);
+	// 		// console.log(max.value);
+
+	// 		if ((min.value && price < min.value) || (max.value && price > max.value)) {
+	// 			card.parentNode.remove();
+	// 			// card.parentNode.style.display = 'none';				
+	// 		} else {
+	// 			goods.appendChild(card.parentNode);
+	// 			// card.parentNode.style.display = '';
+	// 		}
+	// 	});
+	// }
+     
+
+// 	// поиск
+// 	searchBtn.addEventListener('click', () => {
+// 		const searchText = new RegExp(search.value.trim(), 'i'); // i - case insensitive
+// 		console.log(searchText);
+// 		cards.forEach((card) => {
+// 			const title = card.querySelector('.card-title');
+// 			if (!searchText.test(title.textContent)) {
+// 				card.parentNode.style.display = 'none';
+// 			} else {
+// 				card.parentNode.style.display = '';
+
+// 			}
+// 		});
+// 		search.value = '';
+// 	});
 
 }
 // Фильтр акции
